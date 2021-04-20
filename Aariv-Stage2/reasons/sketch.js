08ec125fc7a74eac5acae5015de4c350ce95645f
srushteeMK <76172199+srushteeMK@users.noreply.github.com>
2021-04-21 01:41:57 +0530

    Update sketch.js

--- Aariv-Stage2/reasons/sketch.js
+++ Aariv-Stage2/reasons/sketch.js
@@ -1,8 +1,9 @@
-var title,statsImage
+var title,statsImage,para
 function setup() {
   createCanvas(windowWidth-6,windowHeight);
   title=createElement("h1","REASONS OF GLOBAL WARMING");
         statsImage=createImg("reasonsImg.png")
+        para=createElement("h2","The biggest causes of global warming are deforestation, factories, electricity wastage, etc. Deforestation reduces the number of trees, which means there will be more carbon dioxide in the air. Factories produce a lot of greenhouse gases, and most electricity requires burning fossil fuels, When fossil fuels are burned, they release large amounts of carbon dioxide, a greenhouse gas, into the air. Greenhouse gases trap heat in our atmosphere, causing global warming.")
 }
 
 function draw() {
@@ -11,5 +12,7 @@ function draw() {
         statsImage.position(220, 90)
        statsImage.style("width","70%")
        statsImage.style("height","80%")
+
+       para.position(120,700)
   
 }
